cc7f494c6047f00ab632144cbda089d87eefd1b1
Marcos <marcosugaldemartinez@gmail.com>
2023-09-01 12:22:41 +0200

    fix: fixed error that did not allow to execute sentences correctly

--- spoti-backend/sql/creation.sql
+++ spoti-backend/sql/creation.sql
@@ -1,10 +1,13 @@
-CREATE EXTENSION IF NOT EXISTS 'uuid-ossp';
+DROP TABLE IF EXISTS lists;
+DROP TABLE IF EXISTS users;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS users (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     username TEXT NOT NULL,
-    email TEXT NOR NULL UNIQUE,
-    password TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS lists (
