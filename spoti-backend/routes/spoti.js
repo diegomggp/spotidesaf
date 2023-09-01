bbd5064cca7ea1e12f065d2e57a98472511b4f18
Marcos <marcosugaldemartinez@gmail.com>
2023-09-01 13:24:38 +0200

    feat: successfully created route for adding a new list

--- spoti-backend/routes/spoti.js
+++ spoti-backend/routes/spoti.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
+const spotiControllers = require("../controllers/spoti");
+
 module.exports = (db) => {
+  router.post("/create", spotiControllers.addList(db));
+
   return router;
 };
