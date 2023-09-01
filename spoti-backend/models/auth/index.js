221cce16d68905163b77d355a0793fdeb33891ea
Marcos <marcosugaldemartinez@gmail.com>
2023-09-01 12:54:14 +0200

    feat: added query for getting loged user information

--- spoti-backend/models/auth/index.js
+++ spoti-backend/models/auth/index.js
@@ -1,4 +1,4 @@
-const { insertUser } = require("./queries");
+const { insertUser, selectByEmail } = require("./queries");
 
 const createUser = (db) => async (username, email, password) => {
   try {
@@ -17,6 +17,42 @@ const createUser = (db) => async (username, email, password) => {
   }
 };
 
+const selectUser = (db) => async (email, compareFn) => {
+  try {
+    const user = await db.maybeOne(selectByEmail(email));
+
+    if (!user)
+      return {
+        ok: false,
+        error_code: "wrong_data",
+      };
+
+    const areEqual = await compareFn(user.password);
+
+    if (!areEqual)
+      return {
+        ok: false,
+        error_code: "wrong_data",
+      };
+
+    return {
+      ok: true,
+      content: {
+        email: user.email,
+        username: user.username,
+        id: user.id,
+      },
+    };
+  } catch (error) {
+    console.info("Select user error: ", error.message);
+    return {
+      ok: false,
+      message: error.message,
+    };
+  }
+};
+
 module.exports = {
   createUser,
+  selectUser,
 };
--- spoti-backend/models/auth/queries.js
+++ spoti-backend/models/auth/queries.js
@@ -1,13 +1,20 @@
 const { sql } = require("slonik");
 
 const insertUser = (username, email, password) => sql.unsafe`
-INSERT INTO users (
-    username, email, password
-    ) VALUES (
-    ${username}, ${email}, ${password}
-    );
+    INSERT INTO users (
+        username, email, password
+        ) VALUES (
+        ${username}, ${email}, ${password}
+        );
+`;
+
+const selectByEmail = (email) => sql.unsafe`
+    SELECT email, username, password, id
+    FROM users
+    WHERE email LIKE ${email};
 `;
 
 module.exports = {
   insertUser,
+  selectByEmail,
 };
